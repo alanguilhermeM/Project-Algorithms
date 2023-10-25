66b08874c986200fdf45129458f0008638116085
alanguilhermeM <alanguilherme13@outlook.com>
2023-10-25 17:14:48 -0300

    Finalizado requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,22 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+
+    permanence_students = 0
+    # print(type(permanence_period), type(target_time))
+
+    for period in permanence_period:
+        if period[0] is None or period[1] is None or target_time is None:
+            return None
+
+        if not isinstance(period[0], int) or not isinstance(period[1], int):
+            return None
+
+        if period[0] <= target_time <= period[1]:
+            permanence_students += 1
+
+    return permanence_students
+
+
+permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+target_time = 5
+
+print(study_schedule(permanence_period, target_time))
