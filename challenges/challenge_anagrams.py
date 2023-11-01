4891937e1ba30647e9b16d2a86836473b2564471
alanguilhermeM <alanguilherme13@outlook.com>
2023-11-01 07:02:08 -0300

    Finalizado requisito 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,42 @@
+def merge_sort(letters):
+    if len(letters) <= 1:
+        return letters
+
+    mid = len(letters) // 2
+    left = letters[:mid]
+    right = letters[mid:]
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    left_index, right_index = 0, 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
+
+    result.extend(left[left_index:])
+    result.extend(right[right_index:])
+    return result
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not first_string and not second_string:
+        return ('', '', False)
+
+    def sorted_string(s):
+        return "".join(merge_sort(list(s.lower())))
+
+    sorted_str1 = sorted_string(first_string)
+    sorted_str2 = sorted_string(second_string)
+
+    return (sorted_str1, sorted_str2, sorted_str1 == sorted_str2)
